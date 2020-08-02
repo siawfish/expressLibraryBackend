535b5cc45cd93c8c8a6385c5553ab64158f81a52
Gerald <mcamanor@gmail.com>
2020-08-02 12:50:40 +0000

    Installs body-parser

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 //Create express server
 const server = express()
 
+//Controllers
+fetchBooksController = (req, res) => {
+
+}
+
 //Route
-server.use()
+// server.use(express.static(path.join(__dirname, 'public')))
+
 
 server.listen(3000, '127.0.0.1', ()=>console.log('Server is ready'))
 
