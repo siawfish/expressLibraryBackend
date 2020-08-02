a19ddf46d2d7c199c7d8be66983600a3b461ff5e
Gerald <mcamanor@gmail.com>
2020-08-02 12:39:35 +0000

    Setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+//Create express server
+const server = express()
+
+//Route
+server.use()
+
+server.listen(3000, '127.0.0.1', ()=>console.log('Server is ready'))
+
